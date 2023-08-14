966a058ff84d42d83b77cb731a45ebafa222e68b
Rodrigo Thaler Knolseisen <rtknolseisen@gmail.com>
2023-08-14 20:29:32 -0300

    feature: creates table from compose

--- app/infra/gateway/createIndexForID.ts
+++
@@ -1,8 +0,0 @@
-import sql from "../adapter/db-postgres.ts";
-
-export const createIndexForID = async () => {
-  return await sql`
-    CREATE INDEX IF NOT EXISTS idx_uuid
-    ON people(id)
-  `;
-};
--- app/server.ts
+++ app/server.ts
@@ -1,15 +1,6 @@
 import { router } from "./router.ts";
 import { kv } from "./infra/adapter/cache-kv.ts";
-import { createPeopleTable } from "./infra/gateway/createPeopleTable.ts";
-import { createIndexForID } from "./infra/gateway/createIndexForID.ts";
 import { addPeopleInBatch } from "./controller/addPeopleInBatch.ts";
-
-try {
-  await createPeopleTable();
-  await createIndexForID();
-} catch (error) {
-  console.error("Error trying to create table", error);
-}
 
 const peopleCache = await kv();
 
